26397e4f20cc6bc7ab4fe120b75e99e1838bc684
sky0815a <sky0815a@gmail.com>
2022-10-25 13:33:18 -0400

    Back ground & disk added. Moving motion in progress

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("script linked")
+const diskPink = document.querySelector(".disk1")
+const empites = document.querySelector(".column1")
+
+
+// Fill Listeners
+diskPink.addEventListener('dragstart', dragStart);
+diskPink.addEventListener('dragstart', dragEnd);
+
+// Drag Functions
+function dragStart() {
+    console.log("start");
+}
+
+function dragEnd() {
+    console.log("end")
+}
