717f529791a5c3048e939e01424c85a87caf0cbc
sky0815a <sky0815a@gmail.com>
2022-10-27 01:08:52 -0400

    Disk Tower Move Work

--- script.js
+++ script.js
@@ -12,112 +12,125 @@
 // 3. If array.length = initial array.length (that means all the disk has been moved properly) 
 
 
-const disk1 = document.querySelector(".disk1");
-const disk1Array = [3,2];
-const disk2 = document.querySelector(".disk2");
-const disk2Array = [1];
-const disk3 = document.querySelector(".disk3");
-const disk3Array = [];
+// const disk1 = document.querySelector(".disk1");
+// const disk1Array = [3,2];
+// const disk2 = document.querySelector(".disk2");
+// const disk2Array = [1];
+// const disk3 = document.querySelector(".disk3");
+// const disk3Array = [];
 
-const column1 = document.querySelector(".column1");
-const column2 = document.querySelector(".column2");
-const column3 = document.querySelector(".column3");
+// const column1 = document.querySelector(".column1");
+// const column2 = document.querySelector(".column2");
+// const column3 = document.querySelector(".column3");
 
-// instead of writing each disk code, how can I change it for what User Click?
-// Like diskClick = userClick
-// diskClick.addEventListener('dragstart', dragStart);
-// diskClick.addEventListener('dragstart', dragEnd);
+// // instead of writing each disk code, how can I change it for what User Click?
+// // Like diskClick = userClick
+// // diskClick.addEventListener('dragstart', dragStart);
+// // diskClick.addEventListener('dragstart', dragEnd);
 
-disk1.addEventListener('dragstart', dragStart);
-disk1.addEventListener('dragend', dragEnd);
+// disk1.addEventListener('dragstart', dragStart);
+// disk1.addEventListener('dragend', dragEnd);
 
-disk2.addEventListener('dragstart', dragStart);
-disk2.addEventListener('dragend', dragEnd);
+// disk2.addEventListener('dragstart', dragStart);
+// disk2.addEventListener('dragend', dragEnd);
 
-disk3.addEventListener('dragstart', dragStart);
-disk3.addEventListener('dragend', dragEnd);
+// disk3.addEventListener('dragstart', dragStart);
+// disk3.addEventListener('dragend', dragEnd);
 
-/////////////////////////////////////////////////////
-
-
-// Can I concise the repeated code?
-
-column1.addEventListener('dragover', dragOver); 
-column1.addEventListener('dragenter', dragEnter); 
-column1.addEventListener('dragleave', dragLeave); 
-column1.addEventListener('drop', dragDrop); 
-
-column2.addEventListener('dragover', dragOver); 
-column2.addEventListener('dragenter', dragEnter); 
-column2.addEventListener('dragleave', dragLeave); 
-column2.addEventListener('drop', dragDrop); 
-
-column3.addEventListener('dragover', dragOver); 
-column3.addEventListener('dragenter', dragEnter); 
-column3.addEventListener('dragleave', dragLeave); 
-column3.addEventListener('drop', dragDrop); 
+// /////////////////////////////////////////////////////
 
 
-///////////////////////////////////////////////////////
+// // Can I concise the repeated code?
 
-// Drag Functions
-function dragStart() {
-    this.className += ' hold';
-    setTimeout(() => (this.className = 'invisible'), 0);
-};
+// column1.addEventListener('dragover', dragOver); 
+// column1.addEventListener('dragenter', dragEnter); 
+// column1.addEventListener('dragleave', dragLeave); 
+// column1.addEventListener('drop', dragDrop); 
 
-function dragEnd() {
-    this.className = 'disk1';
-};
+// column2.addEventListener('dragover', dragOver); 
+// column2.addEventListener('dragenter', dragEnter); 
+// column2.addEventListener('dragleave', dragLeave); 
+// column2.addEventListener('drop', dragDrop); 
 
-function dragOver(e) {
-    e.preventDefault();
-}
-
-function dragEnter(e) {
-    e.preventDefault();
-    console.log('entered')
-}
-
-function dragLeave(e) {
-    e.preventDefault();
-    console.log('leave')
-
-}
-
-function dragDrop(e) {
-    e.preventDefault();
-    this.className
-
-}
+// column3.addEventListener('dragover', dragOver); 
+// column3.addEventListener('dragenter', dragEnter); 
+// column3.addEventListener('dragleave', dragLeave); 
+// column3.addEventListener('drop', dragDrop); 
 
 
-// //////////////////////////////////
-// Things I want to make now:
+// ///////////////////////////////////////////////////////
 
-// 1. Align Disks in Center of the column     <=========     Done, through the Office Hour
+// // Drag Functions
+// function dragStart() {
+//     this.className += ' hold';
+//     setTimeout(() => (this.className = 'invisible'), 0);
+// };
+
+// function dragEnd() {
+//     this.className = 'disk1';
+// };
+
+// function dragOver(e) {
+//     e.preventDefault();
+// }
+
+// function dragEnter(e) {
+//     e.preventDefault();
+//     console.log('entered')
+// }
+
+// function dragLeave(e) {
+//     e.preventDefault();
+//     console.log('leave')
+
+// }
+
+// function dragDrop(e) {
+//     e.preventDefault(); 
+//     let newDisk1 = document.createElement('disk1');
+//     column2.appendChild(newDisk1);
+// }
+
+///////// Temp Cloed ////////////
 
 
-// 2. Algin Disk from the Bottom xx pixe of the Column    <=========     Done, through the Office Hour
+(function() {
+    var dragged, listener;
 
-// 3. When disk is Dropped into the other column, I want to remove that Disk DIV from that column to the other colum
-//    Will be done by using Creat & Delete class
+    console.clear();
 
+    dragged = null;
 
-// for example,
+    listener = document.addEventListener;
 
-{/* <div class="column1">
-    <div class="disk1" draggable="true"></div>
-</div>
-<div class="column2"></div>
-<div class="column3"></div>
+    listener("dragstart", (event) => {
+      console.log("start !");
+      return dragged = event.target;
+    });
 
-to be 
+    listener("dragend", (event) => {
+      return console.log("end !");
+    });
 
-<div class="column1"></div>
-<div class="column2">
-    <div class="disk1" draggable="true"></div>
-</div>
-<div class="column3"></div> */}
+    listener("dragover", function(event) {
+      return event.preventDefault();
+    });
 
-// /////////////////////////////////////
+    listener("drop", (event) => {
+      console.log("drop !");
+      event.preventDefault();
+      if (event.target.className === "column2") {
+        dragged.parentNode.removeChild(dragged);
+        return event.target.appendChild(dragged);
+      }
+      else if (event.target.className === "column3") {
+        dragged.parentNode.removeChild(dragged);
+        return event.target.appendChild(dragged);
+      }
+      else if (event.target.className === "column1") {
+        dragged.parentNode.removeChild(dragged);
+        return event.target.appendChild(dragged);
+      }
+    });
+
+  }).call(this);
